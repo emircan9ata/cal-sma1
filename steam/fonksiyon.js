6eec36d314f9004131894c10e1d5c3ee86696925
emircan9ata <147568121+emircan9ata@users.noreply.github.com>
2023-12-04 09:51:32 +0300

    Add files via upload

---
+++ steam/fonksiyon.js
@@ -0,0 +1,30 @@
+const oyunlartwo = document.getElementById("tt")
+
+
+
+
+
+const ilkoyunlar = document.querySelector(".qq")
+
+function next(){
+
+
+
+    ilkoyunlar.classList.add("active")
+    oyunlartwo.classList.remove("active")
+
+    
+
+
+
+}
+
+function back(){
+
+    ilkoyunlar.classList.remove("active")
+    oyunlartwo.classList.add("active")
+
+}
+
+
+
